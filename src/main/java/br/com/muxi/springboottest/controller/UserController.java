71b2279ab4bfc77ea79be8c9188196d01c4fdfd3
Luiz Ardente <lardente@muxi.com.br>
2017-06-12 18:25:05 -0400

    Adding logback configs for ELK.

--- src/main/java/br/com/muxi/springboottest/controller/UserController.java
+++ src/main/java/br/com/muxi/springboottest/controller/UserController.java
@@ -2,7 +2,10 @@ package br.com.muxi.springboottest.controller;
 
 import br.com.muxi.springboottest.dao.entity.User;
 import br.com.muxi.springboottest.dao.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,29 +17,42 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by lardente on 07/06/2017.
  */
-@Controller
-@RequestMapping("/1.0/users")
+@RestController
+@RequestMapping(value = "/1.0/users")
 public class UserController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ResponseEntity<User> response;
 
     @Autowired
     UserService userService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<User> getAllUsers(Pageable pageable) {
+
+        logger.info("[Início]Recuperando Usuários");
+
         Page<User> users = userService.findAll(pageable);
+
+        logger.info("[Fim]Recuperando Usuários");
         return users;
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> getUser(@PathVariable Long id) {
 
+        logger.info("[Início]Recuperando Usuários");
+
         User user = userService.findOne(id);
 
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
+        if (user != null){
+            response = new ResponseEntity<>(user, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
 
+            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("[Fim]Recuperando Usuários");
+        return response;
     }
 }
