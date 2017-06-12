b17e6b2d46eca98a07154a6aaad9e818bd33f3bd
Luiz Ardente <lardente@muxi.com.br>
2017-06-12 15:41:30 -0400

    Adding singular endpoint.

--- src/main/java/br/com/muxi/springboottest/SpringbootTestApplication.java
+++ src/main/java/br/com/muxi/springboottest/SpringbootTestApplication.java
@@ -2,11 +2,6 @@ package br.com.muxi.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringbootTestApplication {
--- src/main/java/br/com/muxi/springboottest/controller/UserController.java
+++ src/main/java/br/com/muxi/springboottest/controller/UserController.java
@@ -3,10 +3,11 @@ package br.com.muxi.springboottest.controller;
 import br.com.muxi.springboottest.dao.entity.User;
 import br.com.muxi.springboottest.dao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +15,28 @@ import org.springframework.web.bind.annotation.*;
  * Created by lardente on 07/06/2017.
  */
 @Controller
-@EnableAutoConfiguration
+@RequestMapping("/1.0/users")
 public class UserController {
-
-    private String response;
 
     @Autowired
     UserService userService;
 
-    @ResponseBody
-    @GetMapping(value = "/1.0/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<User> getAllUsers(Pageable pageable) {
         Page<User> users = userService.findAll(pageable);
         return users;
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+
+        User user = userService.findOne(id);
+
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
